c4036dcfe5fb3ed812d7c3e75fd16b602a63c5a4
unknown <msg016@naver.com>
2021-08-07 19:53:22 +0900

    팝업 창 크기 조정

--- public/background.js
+++ public/background.js
@@ -3,7 +3,7 @@
 chrome.action.onClicked.addListener(function() {
     chrome.tabs.query({active : true, currentWindow: true}, (tabs) => {
         if (tabs[0].url == 'https://game.mahjongsoul.com/index.html') {
-            chrome.windows.create({url: "index.html", type:"popup", width:1170})
+            chrome.windows.create({url: "index.html", type:"popup", width:900, height:580})
         }
     })
 })
