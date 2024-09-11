63b704d120c1c8e941f05df2d9f4b40e4adbe45c
msg016 <msg016@nexon.co.kr>
2024-09-11 09:59:48 +0900

    Remove auto generated code

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
+  publicDir: "public",
   plugins: [svelte()],
 });
