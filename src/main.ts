bda9d04ca367d2ef6061e23ec1a5b3da3f767d21
msg016 <msg016@nexon.co.kr>
2024-09-11 14:30:28 +0900

    Setting build (tsconfig, rollup)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import "./app.css";
-import App from "./App.svelte";
+import "@src/app.css";
+import App from "@src/App.svelte";
 
 const app = new App({
   target: document.getElementById("app")!,
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   publicDir: "public",
-  plugins: [svelte()],
+  plugins: [svelte(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        background: resolve(__dirname, "src/scripts/background.ts"),
+        content: resolve(__dirname, "src/scripts/content.ts"),
+        page: resolve(__dirname, "src/scripts/page.ts"),
+      },
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
 });
